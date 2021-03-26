fe871c0c543056783202a54440a1b1ac87164736
mptelepro <72145569+mptelepro@users.noreply.github.com>
2021-03-26 23:47:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-owner_id = 1575148105
-bot_token = "1582636407:AAGuKzkSdzblLiJKqfPbJ9tFeGQKPi7h_PM"
-bot_id = 1582636407
+owner_id = 1650092910
+bot_token = "1780500515:AAEibG7j9lM6uLRS5gapRe9r4TIYVxFZPPM"
+bot_id = 1780500515
